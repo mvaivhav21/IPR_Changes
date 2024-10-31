419547adacaf1a96b9289961873f32545c406ee8
VaibhavMethi <vaibhavmethi640@gmail.com>
2024-11-01 01:44:17 +0530

    changes in reg_trainer module

--- utils/regression_trainer.py
+++ utils/regression_trainer.py
@@ -1,165 +1,80 @@
-from utils.trainer import Trainer
-from utils.helper import Save_Handle, AverageMeter
-import os
-import sys
-import time
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
 import torch
-from torch import optim
-from torch.utils.data import DataLoader
-from torch.utils.data.dataloader import default_collate
-import logging
-import numpy as np
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from  models.vgg import vgg19
-from datasets.crowd import Crowd
-from losses.bay_loss import Bay_Loss
-from losses.post_prob import Post_Prob
+from torch.nn import functional as F
 
+__all__ = ['vgg19']
+model_urls = {
+    'vgg19': 'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth',
+}
 
-def train_collate(batch):
-    transposed_batch = list(zip(*batch))
-    images = torch.stack(transposed_batch[0], 0)
-    points = transposed_batch[1]  # the number of points is not fixed, keep it as a list of tensor
-    targets = transposed_batch[2]
-    st_sizes = torch.FloatTensor(transposed_batch[3])
-    return images, points, targets, st_sizes
+class VGG(nn.Module):
+    def __init__(self, features):
+        super(VGG, self).__init__()
+        self.features = features
 
+        # Modified reg_layer with parallel paths for multi-scale feature extraction
+        self.reg_layer_3x3 = nn.Sequential(
+            nn.Conv2d(512, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+        
+        self.reg_layer_5x5 = nn.Sequential(
+            nn.Conv2d(512, 128, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True)
+        )
+        
+        self.reg_layer_7x7 = nn.Sequential(
+            nn.Conv2d(512, 128, kernel_size=7, padding=3),
+            nn.ReLU(inplace=True)
+        )
+        
+        # Concatenated output from the parallel paths will be reduced to a single channel
+        self.output_layer = nn.Conv2d(128 * 3, 1, kernel_size=1)
 
-class RegTrainer(Trainer):
-    def setup(self):
-        """initial the datasets, model, loss and optimizer"""
-        args = self.args
-        if torch.cuda.is_available():
-            self.device = torch.device("cuda")
-            self.device_count = torch.cuda.device_count()
-            # for code conciseness, we release the single gpu version
-            assert self.device_count == 1
-            logging.info('using {} gpus'.format(self.device_count))
+    def forward(self, x):
+        # Feature extraction from VGG backbone
+        x = self.features(x)
+        
+        # Upsample the features before applying regression layers
+        x = F.upsample_bilinear(x, scale_factor=2)
+        
+        # Apply parallel convolutional paths
+        x_3x3 = self.reg_layer_3x3(x)
+        x_5x5 = self.reg_layer_5x5(x)
+        x_7x7 = self.reg_layer_7x7(x)
+        
+        # Concatenate along the channel dimension
+        x = torch.cat((x_3x3, x_5x5, x_7x7), dim=1)  # Resulting in 128 * 3 channels
+        
+        # Reduce channels to 1 for density map output
+        x = self.output_layer(x)
+        
+        return torch.abs(x)  # Ensuring density values are non-negative
+
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
         else:
-            raise Exception("gpu is not available")
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
 
-        self.downsample_ratio = args.downsample_ratio
-        self.datasets = {x: Crowd(os.path.join(args.data_dir, x),
-                                  args.crop_size,
-                                  args.downsample_ratio,
-                                  args.is_gray, x) for x in ['train', 'val']}
-        self.dataloaders = {x: DataLoader(self.datasets[x],
-                                          collate_fn=(train_collate
-                                                      if x == 'train' else default_collate),
-                                          batch_size=(args.batch_size
-                                          if x == 'train' else 1),
-                                          shuffle=(True if x == 'train' else False),
-                                          num_workers=args.num_workers*self.device_count,
-                                          pin_memory=(True if x == 'train' else False))
-                            for x in ['train', 'val']}
-        self.model =vgg19()
-        self.model.to(self.device)
-        self.optimizer = optim.Adam(self.model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+cfg = {
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512]
+}
 
-        self.start_epoch = 0
-        if args.resume:
-            suf = args.resume.rsplit('.', 1)[-1]
-            if suf == 'tar':
-                checkpoint = torch.load(args.resume, self.device)
-                self.model.load_state_dict(checkpoint['model_state_dict'])
-                self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-                self.start_epoch = checkpoint['epoch'] + 1
-            elif suf == 'pth':
-                self.model.load_state_dict(torch.load(args.resume, self.device))
-
-        self.post_prob = Post_Prob(args.sigma,
-                                   args.crop_size,
-                                   args.downsample_ratio,
-                                   args.background_ratio,
-                                   args.use_background,
-                                   self.device)
-        self.criterion = Bay_Loss(args.use_background, self.device)
-        self.save_list = Save_Handle(max_num=args.max_model_num)
-        self.best_mae = np.inf
-        self.best_mse = np.inf
-        self.best_count = 0
-
-    def train(self):
-        """training process"""
-        args = self.args
-        for epoch in range(self.start_epoch, args.max_epoch):
-            logging.info('-'*5 + 'Epoch {}/{}'.format(epoch, args.max_epoch - 1) + '-'*5)
-            self.epoch = epoch
-            self.train_eopch()
-            if epoch % args.val_epoch == 0 and epoch >= args.val_start:
-                self.val_epoch()
-
-    def train_eopch(self):
-        epoch_loss = AverageMeter()
-        epoch_mae = AverageMeter()
-        epoch_mse = AverageMeter()
-        epoch_start = time.time()
-        self.model.train()  # Set model to training mode
-
-        # Iterate over data.
-        for step, (inputs, points, targets, st_sizes) in enumerate(self.dataloaders['train']):
-            inputs = inputs.to(self.device)
-            st_sizes = st_sizes.to(self.device)
-            gd_count = np.array([len(p) for p in points], dtype=np.float32)
-            points = [p.to(self.device) for p in points]
-            targets = [t.to(self.device) for t in targets]
-
-            with torch.set_grad_enabled(True):
-                outputs = self.model(inputs)
-                prob_list = self.post_prob(points, st_sizes)
-                loss = self.criterion(prob_list, targets, outputs)
-
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
-
-                N = inputs.size(0)
-                pre_count = torch.sum(outputs.view(N, -1), dim=1).detach().cpu().numpy()
-                res = pre_count - gd_count
-                epoch_loss.update(loss.item(), N)
-                epoch_mse.update(np.mean(res * res), N)
-                epoch_mae.update(np.mean(abs(res)), N)
-
-        logging.info('Epoch {} Train, Loss: {:.2f}, MSE: {:.2f} MAE: {:.2f}, Cost {:.1f} sec'
-                     .format(self.epoch, epoch_loss.get_avg(), np.sqrt(epoch_mse.get_avg()), epoch_mae.get_avg(),
-                             time.time()-epoch_start))
-        model_state_dic = self.model.state_dict()
-        save_path = os.path.join(self.save_dir, '{}_ckpt.tar'.format(self.epoch))
-        torch.save({
-            'epoch': self.epoch,
-            'optimizer_state_dict': self.optimizer.state_dict(),
-            'model_state_dict': model_state_dic
-        }, save_path)
-        self.save_list.append(save_path)  # control the number of saved models
-
-    def val_epoch(self):
-        epoch_start = time.time()
-        self.model.eval()  # Set model to evaluate mode
-        epoch_res = []
-        # Iterate over data.
-        for inputs, count, name in self.dataloaders['val']:
-            inputs = inputs.to(self.device)
-            # inputs are images with different sizes
-            assert inputs.size(0) == 1, 'the batch size should equal to 1 in validation mode'
-            with torch.set_grad_enabled(False):
-                outputs = self.model(inputs)
-                res = count[0].item() - torch.sum(outputs).item()
-                epoch_res.append(res)
-
-        epoch_res = np.array(epoch_res)
-        mse = np.sqrt(np.mean(np.square(epoch_res)))
-        mae = np.mean(np.abs(epoch_res))
-        logging.info('Epoch {} Val, MSE: {:.2f} MAE: {:.2f}, Cost {:.1f} sec'
-                     .format(self.epoch, mse, mae, time.time()-epoch_start))
-
-        model_state_dic = self.model.state_dict()
-        if (2.0 * mse + mae) < (2.0 * self.best_mse + self.best_mae):
-            self.best_mse = mse
-            self.best_mae = mae
-            logging.info("save best mse {:.2f} mae {:.2f} model epoch {}".format(self.best_mse,
-                                                                                 self.best_mae,
-                                                                                 self.epoch))
-            torch.save(model_state_dic, os.path.join(self.save_dir, 'best_model.pth'))
-
-
-
+def vgg19():
+    """VGG 19-layer model (configuration "E")
+        model pre-trained on ImageNet
+    """
+    model = VGG(make_layers(cfg['E']))
+    model.load_state_dict(model_zoo.load_url(model_urls['vgg19']), strict=False)
+    return model
